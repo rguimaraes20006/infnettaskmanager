488d913520c6e20b70e470bf4f4b29c7d524d9e7
Rodrigo Guimarães <rguimaraes@ons.org.br>
2022-10-27 17:11:01 -0300

    commit v0

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,12 +5,38 @@ import routes from '../../Routes'
 import Splash from '../Splash'
 import * as S from './Style'
 
+import {  initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBNmCthCYb4AI3X46-TEJcbH0R7zf_7WO8",
+    authDomain: "infnettaskmanager.firebaseapp.com",
+    projectId: "infnettaskmanager",
+    storageBucket: "infnettaskmanager.appspot.com",
+    messagingSenderId: "685714588033",
+    appId: "1:685714588033:web:91b66d167ee4290a236880",
+    measurementId: "G-XNZ1NWS1KJ"
+  };
+
+
 const App = () => {
+  const analytics = () => {
+    console.log('chamou analytics');
+    const app= initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  };
   const { showSplash, setShowSplash } = useUI()
 
   useEffect(() => {
+    analytics()
     setTimeout(() => setShowSplash(false), 2000)
-  })
+  }, [analytics])
+
+
+
+
 
   return (
     <S.Paper>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import { CssBaseline, StyledEngineProvider, ThemeProvider } from '@mui/material'
-import React from 'react'
+import React  from 'react'
 import ReactDOMClient from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 import { DataProvider } from './Contexts/DataContext/Provider'
 import store, { persistor } from './Store'
 import theme from './theme'
 import App from './Components/App'
 import './index.css'
-
+ 
 const root = document.getElementById('root')
 const container = ReactDOMClient.createRoot(root)
+
 
 container.render(
   <Provider store={store}>
